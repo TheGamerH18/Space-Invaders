5968dca3a382fcff031f901630128642363a3880
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-04-29 18:54:49 +0200

    Added Comments in Alien

--- src/sprite/Alien.java
+++ src/sprite/Alien.java
@@ -6,10 +6,13 @@ public class Alien extends Sprite {
 
     private Bomb bomb;
 
+    // Constructor with the Spawn Position of the Alien
     public Alien(int x, int y){
         initAlien(x, y);
     }
 
+    // Hidden Init
+    // Creating Vars and selecting Icon
     private void initAlien(int x, int y){
         this.x = x;
         this.y = y;
@@ -20,6 +23,7 @@ public class Alien extends Sprite {
         setImage(ii.getImage());
     }
 
+    // Moving Alien to the Left or Right based on Arg
     public void act(int direction){
         x += direction;
     }
@@ -28,17 +32,17 @@ public class Alien extends Sprite {
         return bomb;
     }
 
+    // Bomb Class
     public static class Bomb extends Sprite {
 
         private boolean destroyed;
 
         public Bomb(int x, int y) {
-
             initBomb(x, y);
         }
 
+        // Hidden Init Bomb with Spawn Position, Bomb moved in Board - Sets Image
         private void initBomb(int x, int y) {
-
             setDestroyed(true);
 
             this.x = x;
