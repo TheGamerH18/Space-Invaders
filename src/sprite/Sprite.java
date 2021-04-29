9cf45b414cd7f5dbe1af81911eea63d5ac01994d
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-04-29 18:59:57 +0200

    Added Comments in Sprite

--- src/sprite/Sprite.java
+++ src/sprite/Sprite.java
@@ -27,34 +27,42 @@ public class Sprite {
         this.visible = visible;
     }
 
+    // Sets Image of Sprite
     public void setImage(Image image) {
         this.image = image;
     }
 
+    // Returns Image of Sprite
     public Image getImage() {
         return image;
     }
 
+    // Sets X Coordinate of Sprite
     public void setX(int x){
         this.x = x;
     }
 
+    // Sets Y Coordinate of Sprite
     public void setY(int y) {
         this.y = y;
     }
 
+    // Returns Y Coordinate of Sprite
     public int getY() {
         return y;
     }
 
+    // Returns X Coordinate of Sprite
     public int getX() {
         return x;
     }
 
+    // Sets Dying var
     public void setDying(boolean dying) {
         this.dying = dying;
     }
 
+    // Returns if Sprite is Dying
     public boolean isDying() {
         return dying;
     }
