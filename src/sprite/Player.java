558bfc99eaf70a03ccc02df2ba97f64fcefa901b
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-04-29 18:49:36 +0200

    Fixing Movement Speed Bug

--- src/sprite/Player.java
+++ src/sprite/Player.java
@@ -33,7 +33,7 @@ public class Player extends Sprite{
 
     public void keyPressed(KeyEvent e){
         int key = e.getKeyCode();
-        if(key == KeyEvent.VK_LEFT) dx -= 2;
+        if(key == KeyEvent.VK_LEFT) dx = -2;
         if(key == KeyEvent.VK_RIGHT) dx = 2;
     }
 
