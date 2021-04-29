c191112faba2b59cee6d8fc5ff09f71433b8a8ff
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-04-29 18:57:26 +0200

    Added Comments in Shot

--- src/sprite/Shot.java
+++ src/sprite/Shot.java
@@ -7,10 +7,12 @@ public class Shot extends Sprite {
     public Shot() {
 
     }
+
     public Shot(int x, int y){
         initShot(x, y);
     }
 
+    // Hidden Init Setting Position of Shot. Movement in Board
     private void initShot(int x, int y){
         ImageIcon ii = new ImageIcon("src/images/shot.png");
         setImage(ii.getImage());
