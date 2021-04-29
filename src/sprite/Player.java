8912297f909ffbcbca1429aabbfc7a0ce73fb554
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-04-29 18:56:42 +0200

    Added Comments in Player

--- src/sprite/Player.java
+++ src/sprite/Player.java
@@ -13,6 +13,7 @@ public class Player extends Sprite{
         initPlayer();
     }
 
+    // Hidden Init Setting start Position and Image
     private void initPlayer() {
         ImageIcon ii = new ImageIcon("src/images/player.png");
         width = ii.getImage().getWidth(null);
@@ -25,18 +26,21 @@ public class Player extends Sprite{
         setY(START_Y);
     }
 
+    // Moving player based on dx var (Changed by Pressing key)
     public void act() {
         x += dx;
         if(x <= 2) x = 2;
         if(x >= Commons.BOARD_WIDTH - 2 * width) x = Commons.BOARD_WIDTH - 2 * width;
     }
 
+    // Called when a key is pressed. Sets dx var for Movement
     public void keyPressed(KeyEvent e){
         int key = e.getKeyCode();
         if(key == KeyEvent.VK_LEFT) dx = -2;
         if(key == KeyEvent.VK_RIGHT) dx = 2;
     }
 
+    // Called when a key is released. Sets dx var to end Movement
     public void keyReleased(KeyEvent e){
         int key = e.getKeyCode();
         if(key == KeyEvent.VK_LEFT || key == KeyEvent.VK_RIGHT) dx = 0;
