3c436c1e5eef9113befff579d5b7af3ae416ada8
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-05-01 14:55:27 +0200

    Adding Multiplayer Button in Main Menu

--- src/client/Menu.java
+++ src/client/Menu.java
@@ -28,16 +28,23 @@ public class Menu extends JPanel {
         title.setBounds(Commons.BOARD_WIDTH / 2 - titlesize.stringWidth(GameName) / 2,50, titlesize.stringWidth(GameName), titlesize.getHeight());
         add(title);
 
+        // Creating Multiplayer Button
+        MultiplayerGame = new MenuButton("Multiplayer");
+        Dimension size = MultiplayerGame.getPreferredSize();
+        MultiplayerGame.setBounds((Commons.BOARD_WIDTH / 2) - size.width / 2, Commons.BOARD_HEIGHT / 2 - size.height / 2, size.width, size.height);
+        MultiplayerGame.addActionListener(new MenuCycle());
+        add(MultiplayerGame);
+
         // Creating start button
         startgame = new MenuButton("Start Game");
-        Dimension size = startgame.getPreferredSize();
         startgame.setBounds((Commons.BOARD_WIDTH / 2) - size.width / 2, Commons.BOARD_HEIGHT / 2 - size.height * 2, size.width, size.height);
         startgame.addActionListener(new MenuCycle());
         add(startgame);
 
+
         // Creating exit button
         ExitGame = new MenuButton("Exit Game");
-        ExitGame.setBounds((Commons.BOARD_WIDTH / 2) - size.width / 2, Commons.BOARD_HEIGHT / 2 - size.height / 2, size.width, size.height);
+        ExitGame.setBounds((Commons.BOARD_WIDTH / 2) - size.width / 2, Commons.BOARD_HEIGHT / 2 + size.height * 2, size.width, size.height);
         ExitGame.addActionListener(new MenuCycle());
         add(ExitGame);
     }
