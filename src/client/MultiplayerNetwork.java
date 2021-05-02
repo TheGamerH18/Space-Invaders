34e467bde97ce37c83b9b474cbb4109270875129
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-05-02 20:51:00 +0200

    New Methods for syncing Players

--- src/client/MultiplayerNetwork.java
+++ src/client/MultiplayerNetwork.java
@@ -5,29 +5,41 @@ import com.blogspot.debukkitsblog.net.Datapackage;
 import com.blogspot.debukkitsblog.net.Executable;
 
 import java.net.Socket;
-import java.util.List;
 
 public class MultiplayerNetwork extends Client {
+    int playeramount = 2;
 
+    private int[][] playerpos = new int[playeramount][2];
+    private int gameinfo;
 
     public MultiplayerNetwork(String host, String username) {
         super(host, 25598, 1000, false, username, "player");
+        setMuted(true);
+
+        registerMethod("GAME_INFO", new Executable() {
+            @Override
+            public void run(Datapackage pack, Socket socket) {
+                gameinfo = (int) pack.get(1);
+            }
+        });
 
         registerMethod("POS", new Executable() {
             @Override
             public void run(Datapackage pack, Socket socket) {
-            }
-        });
-
-        registerMethod("PLAYERS", new Executable() {
-            @Override
-            public void run(Datapackage pack, Socket socket) {
+                playerpos[0][0] = (int) pack.get(1);
+                playerpos[0][1] = (int) pack.get(2);
+                playerpos[1][0] = (int) pack.get(3);
+                playerpos[1][1] = (int) pack.get(4);
             }
         });
         start();
     }
 
+    public int[][] getPlayerpos() {
+        return playerpos;
     }
 
+    public int getGameinfo() {
+        return gameinfo;
     }
 }
