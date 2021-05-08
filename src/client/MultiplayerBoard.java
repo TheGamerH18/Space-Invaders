46cdd255e30bca5ba41af0419f452bc954597c60
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-05-09 00:52:28 +0200

    Optimizing Imports

--- src/client/MultiplayerBoard.java
+++ src/client/MultiplayerBoard.java
@@ -1,7 +1,6 @@
 package client;
 
 import com.blogspot.debukkitsblog.net.Datapackage;
-import sprite.Alien;
 import sprite.Player;
 import sprite.Shot;
 
@@ -11,7 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.List;
+import java.util.ArrayList;
 
 public class MultiplayerBoard extends JPanel {
 
