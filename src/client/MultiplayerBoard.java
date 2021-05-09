e8fe95fd06710fff8c6e49e78f467de831f0e725
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-05-09 19:39:15 +0200

    Fixing shots, when player is not alive
    Players can not trigger any actions if they are not visible

--- src/client/MultiplayerBoard.java
+++ src/client/MultiplayerBoard.java
@@ -79,12 +79,14 @@ public class MultiplayerBoard extends JPanel {
 
         @Override
         public void keyPressed(KeyEvent e) {
-            players[myplayerid].keyPressed(e);
-            int key = e.getKeyCode();
-            if (key == KeyEvent.VK_SPACE) {
-                int x = players[myplayerid].getX();
-                int y = players[myplayerid].getY();
-                network.sendMessage(new Datapackage("NEW_SHOT", x, y, myplayerid));
+            if(players[myplayerid].isVisible()) {
+                players[myplayerid].keyPressed(e);
+                int key = e.getKeyCode();
+                if (key == KeyEvent.VK_SPACE) {
+                    int x = players[myplayerid].getX();
+                    int y = players[myplayerid].getY();
+                    network.sendMessage(new Datapackage("NEW_SHOT", x, y, myplayerid));
+                }
             }
         }
     }
